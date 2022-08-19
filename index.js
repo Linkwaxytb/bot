1c6450e00cabab8979c2f57a16558f16119b6e3f
Linkwax-55210 <86116531+Linkwax-55210@users.noreply.github.com>
2022-08-19 23:12:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ const RoiLion = new EmbedBuilder()
 //espace personnalisation des réponse au modal
 client.on('interactionCreate', interaction => {
 	if (interaction.type !== InteractionType.ModalSubmit) return;
-    
+     console.log('Test')
 	
 	var favoriteColor = interaction.fields.getTextInputValue('favoriteColorInput');
 
