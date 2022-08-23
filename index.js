885419559c2d4778b196238a452f83c956ab10ac
Linkwaxytb <86116531+Linkwaxytb@users.noreply.github.com>
2022-08-24 00:46:11 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -13,16 +13,7 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord.js');
 const clientId = '996779963720146955';
 const guildId = '971302837080432682';
-var express = require('express');
-var app = express();
-var path = require('path');
-
-
-app.use(express.static(__dirname + '/'));
-app.get('*', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, './src/index.html'));
-});
-app.listen(process.env.PORT || 8080);
+const PORT = process.env.PORT || 3000;
 const commands = [
 	new SlashCommandBuilder().setName('modfilms').setDescription('Menu de films!'),
 ]
